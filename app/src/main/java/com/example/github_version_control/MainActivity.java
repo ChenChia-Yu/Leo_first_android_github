a81b2f49e77156ff2bc1f015074edfe307f36097
Leo <a0979663740@gmail.com>
2021-02-24 20:25:42 +0800

    v1

--- app/src/main/java/com/example/github_version_control/MainActivity.java
+++ app/src/main/java/com/example/github_version_control/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.github_version_control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("Hello","This is Leo Chen");
     }
 }
